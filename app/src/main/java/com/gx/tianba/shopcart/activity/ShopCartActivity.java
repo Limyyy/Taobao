3df86a956aded67dcc307ef76130d6efd7aa0836
金子射爱情、 <1569973744@qq.com>
2018-11-28 20:50:36 +0800

    购物车界面设置

---
+++ app/src/main/java/com/gx/tianba/shopcart/activity/ShopCartActivity.java
@@ -0,0 +1,16 @@
+package com.gx.tianba.shopcart.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gx.tianba.R;
+
+public class ShopCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_cart);
+    }
+
+}
